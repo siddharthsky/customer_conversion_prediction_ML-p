d6e520cd6d0eef29cdf01618bd6fce342eca9101
Siddharth Kamble <siddharthsk7t@gmail.com>
2023-04-08 00:02:16 +0530

    - train pipeline

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,5 +1,5 @@
-from src.logger import logging
-from src.exception import CustomException
 import os
 import sys
 sys.path.append(os.getcwd())
+from src.logger import logging
+from src.exception import CustomException
