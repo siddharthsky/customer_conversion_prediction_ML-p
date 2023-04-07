4e01218dbe9fbad1d8fc617cf19febd8ecf032eb
Siddharth Kamble <siddharthsk7t@gmail.com>
2023-04-08 00:00:05 +0530

    -

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,3 +1,11 @@
+from src.utils import load_object
+import numpy as np
+import pandas as pd
+from src.logger import logging
+from src.exception import CustomException
+import os
+import sys
+sys.path.append(os.getcwd())
 
 
 class PredictPipeline:
